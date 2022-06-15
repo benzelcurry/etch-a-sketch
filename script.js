9b2f319f3c85e47a500ee9e4da0df7072aa508b5
Ben <benawhiting@gmail.com>
2022-06-14 18:45:23 -0700

    Create mouseover effect for grid

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+
+
 // Create 16x16 grid
 function makeGrid(size) {
     let canvas = document.querySelector("#board");
     canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-    let rows = document.createElement("div");
-    rows.classList.add("rows");
-    rows.style.backgroundColor = "cyan";
-    rows.textContent = "Test";
-
-    let columns = document.createElement("div");
-    columns.classList.add("columns");
-
     for (let i = 0; i < 256; i++) {
-        board.appendChild(rows.cloneNode(true));
+        let squares = document.createElement("div");
+        squares.classList.add("squares");
+        squares.style.backgroundColor = "cyan";
+        squares.textContent = "x";
+        board.appendChild(squares);
+
+        // Turns squares black upon cursor hover
+        squares.addEventListener("mouseover", () => {
+            squares.style.backgroundColor = "black";
+        });
     }
 }
 
