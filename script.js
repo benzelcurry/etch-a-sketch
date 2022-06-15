48b6ade52ff15f45103d8c9f32a7ca3527f135ad
Ben <benawhiting@gmail.com>
2022-06-14 20:09:18 -0700

    Create button for user customization

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
+let size = 16;
 
+// Gets size input from user
+function getSize() {
+    userSize = Number(prompt("Please enter your desired height & width for the grid (limit: 100)."));
 
-// Create 16x16 grid
+    if (typeof(userSize) !== "number" || userSize < 0 || userSize > 100 || typeof(userSize) == "NaN") {
+        alert("Please follow the instructions.");
+    } else {
+        console.log(userSize);
+        return userSize;
+    }
+}
+
+// Create grid
 function makeGrid(size) {
     let canvas = document.querySelector("#board");
-    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    const button = document.querySelector("#btn");
 
-    for (let i = 0; i < 256; i++) {
+    // Sets the amount of columns and rows in the grid
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    // Fills the grid with squares
+    for (let i = 0; i < (size ** 2); i++) {
         let squares = document.createElement("div");
         squares.classList.add("squares");
-        squares.style.backgroundColor = "cyan";
-        squares.textContent = "x";
+        squares.style.backgroundColor = "white";
         board.appendChild(squares);
 
         // Turns squares black upon cursor hover
         squares.addEventListener("mouseover", () => {
             squares.style.backgroundColor = "black";
+            console.log(size);
         });
     }
+
+    button.addEventListener("click", () => {
+        getSize();
+        size = userSize;
+        
+        // Removes children from the canvas
+        while (board.firstChild) {
+            board.removeChild(board.lastChild);
+        }
+
+        // Draws a new grid
+        makeGrid(size);
+    });
+
 }
 
 makeGrid(16);
