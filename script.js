6bb6008e9d84f316ce10b0331ee931eda280010a
Ben <benawhiting@gmail.com>
2022-06-14 17:58:48 -0700

    Finish makeGrid()

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // Create 16x16 grid
-function makeGrid() {
-    let grid = document.querySelector("#container");
+function makeGrid(size) {
+    let canvas = document.querySelector("#board");
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
     let rows = document.createElement("div");
     rows.classList.add("rows");
+    rows.style.backgroundColor = "cyan";
     rows.textContent = "Test";
 
     let columns = document.createElement("div");
     columns.classList.add("columns");
 
-    for (let i = 0; i < 16; i++) {
-        container.appendChild(rows.cloneNode(true));
-        console.log(i);
+    for (let i = 0; i < 256; i++) {
+        board.appendChild(rows.cloneNode(true));
     }
 }
+
+makeGrid(16);
