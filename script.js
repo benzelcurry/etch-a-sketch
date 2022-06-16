c12255cc34411c9d07a163e44a542d07bfde6855
Ben <benawhiting@gmail.com>
2022-06-15 19:58:49 -0700

    Fix minor bug and add color feature

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
 let size = 16;
+let colorStatus = false;
+const button = document.querySelector("#btn");
+const colors = document.querySelector('#colors');
+
+button.addEventListener("click", () => {
+    // Removes children from the canvas
+   while (board.firstChild) {
+       board.removeChild(board.lastChild);
+   }
+   
+   getSize();
+});
+
+colors.addEventListener("click", () => {
+    if (colorStatus == false) {
+        colorStatus = true;
+    } else {
+        colorStatus = false;
+    }
+});
 
 // Gets size input from user
 function getSize() {
     userSize = Number(prompt("Please enter your desired height & width for the grid (limit: 100)."));
 
-    if (typeof(userSize) !== "number" || userSize < 0 || userSize > 100 || typeof(userSize) == "NaN") {
+    if (typeof(userSize) !== "number" || userSize < 0 || userSize > 100) {
         alert("Please follow the instructions.");
     } else {
-        console.log(userSize);
-        return userSize;
+        makeGrid(userSize);
     }
 }
 
 // Create grid
 function makeGrid(size) {
     let canvas = document.querySelector("#board");
-    const button = document.querySelector("#btn");
-
+ 
     // Sets the amount of columns and rows in the grid
     canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     // Fills the grid with squares
     for (let i = 0; i < (size ** 2); i++) {
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
         let squares = document.createElement("div");
-        squares.classList.add("squares");
         squares.style.backgroundColor = "white";
         board.appendChild(squares);
-
-        // Turns squares black upon cursor hover
-        squares.addEventListener("mouseover", () => {
-            squares.style.backgroundColor = "black";
-            console.log(size);
-        });
-    }
-
-    button.addEventListener("click", () => {
-        getSize();
-        size = userSize;
         
-        // Removes children from the canvas
-        while (board.firstChild) {
-            board.removeChild(board.lastChild);
-        }
-
-        // Draws a new grid
-        makeGrid(size);
-    });
-
+        // Set color of squares upon hovering with cursor
+        squares.addEventListener("mouseover", () => {
+            if (colorStatus == false) {
+                squares.style.backgroundColor = "black";
+            } else {
+                squares.style.backgroundColor = `#${randomColor}`;
+            }
+        });
+    }       
 }
 
-makeGrid(16);
+makeGrid(size);
